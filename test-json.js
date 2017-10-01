fdc917aa7251b97ef1ad396386b8a985348ff03f
Peterard <32120920+Peterard@users.noreply.github.com>
2017-10-01 19:14:59 +0900

    Add files via upload

--- test-json.js
+++ test-json.js
@@ -1,1 +1,1 @@
-{{"x":11,"y":33,"details":{"test":"test1","object":"object1"}},{"x":55,"y":99,"details":"DETAILS"}}
+{"x":11,"y":33,"details":"DETAILS2","test":"test1","object":"object1","x":55,"y":99,"details":"DETAILS"}
