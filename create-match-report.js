ba032b5281cf3348dd8545eb0fc1d901d3d143b2
Peterard <32120920+Peterard@users.noreply.github.com>
2017-10-07 11:56:37 +0900

    Add files via upload

--- create-match-report.js
+++ create-match-report.js
@@ -8,11 +8,70 @@ function createMatchReport(){
                   "http://nflrt.com/wp-content/uploads/2014/09/Patriots-FC.png"];
   var date = "DD/MM/YYYY";
   var competitionName = "TEST COMPETITION";
+  var teamListHome = [{name:"Artur Boruc", position:"Keeper", status:"starter", number:"22"},
+                      {name:"Artur Boruc", position:"Defender", status:"starter", number:"22"},
+                    {name:"Artur Boruc", position:"Defender", status:"starter", number:"22"},
+                  {name:"Artur Boruc", position:"Defender", status:"starter", number:"22"},
+                {name:"Artur Boruc", position:"Midfielder", status:"starter", number:"22"},
+                {name:"Artur Boruc", position:"Midfielder", status:"starter", number:22},
+                {name:"Artur Boruc", position:"Midfielder", status:"starter", number:22},
+                {name:"Artur Boruc", position:"Midfielder", status:"starter", number:22},
+                {name:"Artur Boruc", position:"Midfielder", status:"starter", number:22},
+                {name:"Artur Boruc", position:"Forward", status:"starter", number:22},
+                {name:"Artur Boruc", position:"Forward", status:"starter", number:22},
+                {name:"Artur Boruc", position:"Forward", status:"starter", number:22},
+                {name:"Artur Boruc", position:"Forward", status:"sub", number:22},
+                {name:"Artur Boruc", position:"Forward", status:"sub", number:22},
+                {name:"Artur Boruc", position:"Forward", status:"sub", number:22},
+                {name:"Artur Boruc", position:"Forward", status:"sub", number:22},
+                {name:"Artur Boruc", position:"Forward", status:"sub", number:22},
+                {name:"Artur Boruc", position:"Forward", status:"sub", number:22},
+                {name:"Artur Boruc", position:"Forward", status:"squad", number:22},
+                {name:"Artur Boruc", position:"Forward", status:"squad", number:22},
+                {name:"Artur Boruc", position:"Forward", status:"squad", number:22},
+                {name:"Artur Boruc", position:"Forward", status:"squad", number:22},
+                {name:"Artur Boruc", position:"Forward", status:"squad", number:22},
+                {name:"Artur Boruc", position:"Forward", status:"squad", number:22},
+                {name:"Artur Boruc", position:"Forward", status:"squad", number:22}];
+
+
+           var teamListAway = [{name:"Sasa Papac", position:"Keeper", status:"starter", number:22},
+                     {name:"Sasa Papac", position:"Defender", status:"starter", number:22},
+                   {name:"Sasa Papac", position:"Defender", status:"starter", number:22},
+                 {name:"Sasa Papac", position:"Defender", status:"starter", number:22},
+               {name:"Sasa Papac", position:"Midfielder", status:"starter", number:22},
+               {name:"Sasa Papac", position:"Midfielder", status:"starter", number:22},
+               {name:"Sasa Papac", position:"Midfielder", status:"starter", number:22},
+               {name:"Sasa Papac", position:"Midfielder", status:"starter", number:22},
+               {name:"Sasa Papac", position:"Midfielder", status:"starter", number:22},
+               {name:"Sasa Papac", position:"Forward", status:"starter", number:22},
+               {name:"Sasa Papac", position:"Forward", status:"starter", number:22},
+               {name:"Sasa Papac", position:"Forward", status:"starter", number:22},
+               {name:"Sasa Papac", position:"Forward", status:"sub", number:22},
+               {name:"Sasa Papac", position:"Forward", status:"sub", number:22},
+               {name:"Sasa Papac", position:"Forward", status:"sub", number:22},
+               {name:"Sasa Papac", position:"Forward", status:"sub", number:22},
+               {name:"Sasa Papac", position:"Forward", status:"sub", number:22},
+               {name:"Sasa Papac", position:"Forward", status:"sub", number:22},
+               {name:"Sasa Papac", position:"Forward", status:"squad", number:22},
+               {name:"Sasa Papac", position:"Forward", status:"squad", number:22},
+               {name:"Sasa Papac", position:"Forward", status:"squad", number:22},
+               {name:"Sasa Papac", position:"Forward", status:"squad", number:22},
+               {name:"Sasa Papac", position:"Forward", status:"squad", number:22},
+               {name:"Sasa Papac", position:"Forward", status:"squad", number:22},
+               {name:"Sasa Papac", position:"Forward", status:"squad", number:22}];
   var score = "3-1";
   var wordForManager = "Manager";
   var wordForReferee = "Referee";
   var wordForStadium = "Stadium";
   var wordForTime = "Total Time";
+  var wordForTeamSheet = "Teamsheet";
+  var wordForHomeStarters = "Home starters";
+  var wordForHomeSubs = "Home substitutions";
+  var wordForHomeSquad = "Home squad";
+  var wordForAwayStarters = "Away starters";
+  var wordForAwaySubs = "Away substitutions";
+  var wordForAwaySquad = "Away squad";
   var wordForGoal = '<div class="icon-holder football-icon-holder"><img src="football.png" class="event-icon event-icon-football" alt="Goal" style="min-height:9.73px;min-width:9.73px;max-height:17.5px;max-width:17.5px;width:1.46vw;height:1.46vw;"></div>';
   var wordForRedCard = '<div class="icon-holder red-card-icon-holder"><img class="event-icon event-icon-red-card"  src="220px-Red_card.svg.png" alt="Red Card" style="min-height:10px;min-width:7.67px;max-height:18px;max-width:13.8px;width:1.15vw;height:1.5vw;"></div>';
   var wordForPenalty = "Penalty";
@@ -22,10 +81,10 @@ function createMatchReport(){
   var timeElapsedSecondHalf = 48;
   var totalTimeElapsed = timeElapsedFirstHalf+timeElapsedSecondHalf;
   var matchEvents = [{eventType:"goal", side:"home", player:"Bandrea Birlo", min:22},
-                      {eventType:"red-card", side:"away", player:"Venegoor Of Hesselink", min:29},
-                      {eventType:"penalty-goal", side:"away", player:"Lamine Zemmamouche", min:44},
-                      {eventType:"red-card", side:"home", player:"Pangel Pi Paria", min:75},
-                      {eventType:"goal", side:"away", player:"Miego Morlan", min:85}];
+            {eventType:"red-card", side:"away", player:"Venegoor Of Hesselink", min:29},
+            {eventType:"penalty-goal", side:"away", player:"Lamine Zemmamouche", min:44},
+            {eventType:"red-card", side:"home", player:"Pangel Pi Paria", min:75},
+            {eventType:"goal", side:"away", player:"Miego Morlan", min:85}];
 
   var matchReportHeader = '<div id="match-details">\
     <div id="primary-match-details">\
@@ -101,12 +160,101 @@ function createMatchReport(){
   $("#match-events").empty();
   $("#match-events").append(events);
 
-  var teamSheets = "<div id='teamsheets'>Teamsheets"+ $(".event-text").css("width") +" : "+ window.outerWidth +"</div>";
+  var teamSheets = "<div id='teamsheets'>"+wordForTeamSheet+ $(".event-text").css("width") +" : "+ window.outerWidth +"</div>";
+
+  var homeStarters = "<div id='home-teamsheet-header'>"+teamNames[0]+" "
+                      +wordForTeamSheet+"</div>" + '<div id="home-teamsheet-starters-header">'
+                      +wordForHomeStarters+'</div>';
+  var homeSubs = "";
+  var homeSquad = '<div id="home-teamsheet-squad-header">'
+  +wordForHomeSquad+'</div>';
+
+  for(var teamSheetCounter = 0; teamSheetCounter < teamListHome.length; teamSheetCounter++){
+    if(teamListHome[teamSheetCounter]["status"] === "starter"){
+        homeStarters = homeStarters + '<div class="home-teamsheet-starter-row">'+
+                                '<div class="home-teamsheet-text home-teamsheet-starter home-teamsheet-starter-number">'+
+                                teamListHome[teamSheetCounter]["number"]+"</div>"+
+                                '<div class="home-teamsheet-text home-teamsheet-starter home-teamsheet-starter-position">'+
+                                teamListHome[teamSheetCounter]["position"]+"</div>"+
+                                '<div class="home-teamsheet-text home-teamsheet-starter home-teamsheet-starter-name">'
+                                +teamListHome[teamSheetCounter]["name"]+'</div>' +'</div>';
+    }
+
+    if(teamListHome[teamSheetCounter]["status"] === "sub"){
+      if(homeSubs.length < 1){
+        homeSubs = homeSubs + '<div id="home-teamsheet-subs-header">'
+        +wordForHomeSubs+'</div>';
+      }
+        homeSubs = homeSubs + '<div class="home-teamsheet-sub-row">'+
+                                '<div class="home-teamsheet-text home-teamsheet-sub home-teamsheet-sub-number">'+
+                                teamListHome[teamSheetCounter]["number"]+"</div>"+
+                                '<div class="home-teamsheet-text home-teamsheet-sub home-teamsheet-sub-position">'+
+                                teamListHome[teamSheetCounter]["position"]+"</div>"+
+                                '<div class="home-teamsheet-text home-teamsheet-sub home-teamsheet-sub-name">'
+                                +teamListHome[teamSheetCounter]["name"]+'</div>' +'</div>';
+    }
+
+    if(teamListHome[teamSheetCounter]["status"] === "squad"){
+        homeSquad = homeSquad + '<div class="home-teamsheet-squad-row">'+
+                                '<div class="home-teamsheet-text home-teamsheet-squad home-teamsheet-squad-number">'+
+                                teamListHome[teamSheetCounter]["number"]+"</div>"+
+                                '<div class="home-teamsheet-text home-teamsheet-squad home-teamsheet-squad-position">'+
+                                teamListHome[teamSheetCounter]["position"]+"</div>"+
+                                '<div class="home-teamsheet-text home-teamsheet-squad home-teamsheet-squad-name">'
+                                +teamListHome[teamSheetCounter]["name"]+'</div>' +'</div>';
+    }
+  }
+
+
+    var awayStarters = "<div id='away-teamsheet-header'>"+teamNames[1]+" "
+                        +wordForTeamSheet+"</div>" + '<div id="away-teamsheet-starters-header">'
+                        +wordForAwayStarters+'</div>';
+    var awaySubs = "";
+    var awaySquad = '<div id="away-teamsheet-squad-header">'
+    +wordForAwaySquad+'</div>';
+
+
+  for(var teamSheetCounter = 0; teamSheetCounter < teamListAway.length; teamSheetCounter++){
+    if(teamListAway[teamSheetCounter]["status"] === "starter"){
+        awayStarters = awayStarters + '<div class="away-teamsheet-starter-row">'+
+                                '<div class="away-teamsheet-text away-teamsheet-starter away-teamsheet-starter-number">'+
+                                teamListAway[teamSheetCounter]["number"]+"</div>"+
+                                '<div class="away-teamsheet-text away-teamsheet-starter away-teamsheet-starter-position">'+
+                                teamListAway[teamSheetCounter]["position"]+"</div>"+
+                                '<div class="away-teamsheet-text away-teamsheet-starter away-teamsheet-starter-name">'
+                                +teamListAway[teamSheetCounter]["name"]+'</div>' +'</div>';
+    }
+
+    if(teamListAway[teamSheetCounter]["status"] === "sub"){
+      if(awaySubs.length < 1){
+        awaySubs = awaySubs + '<div id="away-teamsheet-subs-header">'
+        +wordForAwaySubs+'</div>';
+      }
+        awaySubs = awaySubs + '<div class="away-teamsheet-sub-row">'+
+                                '<div class="away-teamsheet-text away-teamsheet-sub away-teamsheet-sub-number">'+
+                                teamListAway[teamSheetCounter]["number"]+"</div>"+
+                                '<div class="away-teamsheet-text away-teamsheet-sub away-teamsheet-sub-position">'+
+                                teamListAway[teamSheetCounter]["position"]+"</div>"+
+                                '<div class="away-teamsheet-text away-teamsheet-sub away-teamsheet-sub-name">'
+                                +teamListAway[teamSheetCounter]["name"]+'</div>' +'</div>';
+    }
+
+    if(teamListAway[teamSheetCounter]["status"] === "squad"){
+        awaySquad = awaySquad + '<div class="away-teamsheet-squad-row">'+
+                               '<div class="away-teamsheet-text away-teamsheet-squad away-teamsheet-squad-number">'+
+                                teamListAway[teamSheetCounter]["number"]+"</div>"+
+                                '<div class="away-teamsheet-text away-teamsheet-squad away-teamsheet-squad-position">'+
+                                teamListAway[teamSheetCounter]["position"]+"</div>"+
+                                '<div class="away-teamsheet-text away-teamsheet-squad away-teamsheet-squad-name">'
+                                +teamListAway[teamSheetCounter]["name"]+'</div>' +'</div>';
+    }
+  }
+
+  teamSheets = teamSheets + homeStarters + homeSubs + homeSquad
+                + awayStarters + awaySubs + awaySquad;
 
   $("#match-events").append(teamSheets);
 
   resizeMatchReport();
-
-  $("#match-events").append(teamSheets);
 
 }
